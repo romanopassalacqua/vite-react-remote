a765cce63f89949ff85a51d43b2d19738a4a18f7
Romano Passalacqua <romano.passalacqua@pst.asseco.com>
2023-07-13 08:23:28 +0100

    fix: update react import

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import React from 'react'
 
 const Counter = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 }
